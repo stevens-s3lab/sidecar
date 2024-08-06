6356d916f7dfc39a51d73c0ea9fdb5ecad932648
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-08-06 02:24:17 -0400

    Fix path error on asan pass.

--- sidecar-llvm/llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
+++ sidecar-llvm/llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
@@ -88,7 +88,7 @@
 #include <string>
 #include <tuple>
 // S3LAB
-#include "../../../../../coresight_utils/monitor/asan_decouple.h"
+#include "../../../../../sidecar-monitors/sideasan/asan_decouple.h"
 
 
 using namespace llvm;
