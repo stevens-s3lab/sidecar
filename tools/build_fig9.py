91e32cc6b9e8d1ac5bc6c0a2714e9c3254ae171d
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-08-26 18:15:31 -0400

    Call all build scripts.

--- tools/build_fig9.py
+++ tools/build_fig9.py
@@ -1,15 +1,7 @@
 import subprocess
 
 # List of values to use as arguments
-build_types = [
-    'lto',
-    'cfi',
-    'sidecfi',
-    'safestack',
-    'sidestack',
-    'asan',
-    'sideasan'
-]
+build_types = ["lto", "cfi", "sidecfi", "safestack", "sidestack", "asan", "sideasan"]
 
 # Loop over each build type and execute the script
 for build_type in build_types:
@@ -17,16 +9,15 @@ for build_type in build_types:
         print(f"Building lighttpd with {build_type}...")
         # Execute the script with the current build type
         result = subprocess.run(
-            ['./tools/build_lighttpd.sh', build_type, 'all'],
+            ["./tools/build_lighttpd.sh", build_type, "all"],
             check=True,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            stderr=subprocess.PIPE,
         )
         print(f"Build completed for {build_type}.\n")
-        print(result.stdout.decode('utf-8'))
+        print(result.stdout.decode("utf-8"))
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while building {build_type}.")
-        print(e.stderr.decode('utf-8'))
+        print(e.stderr.decode("utf-8"))
 
 print("All builds completed.")
-
