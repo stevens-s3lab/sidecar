bb8539f91f771a013030385d7241f8a4790543e4
kleftog <kkleftog@gmail.com>
2024-08-26 17:54:29 -0400

    Build file for figure 9.

--- tools/build_lighttpd.sh
+++ tools/build_lighttpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <lto | cfi | sidecfi | sidexcfi> <envsetup | ssl | pcre | app | run | all>"
+    echo "Usage: $0 <lto | cfi | sidecfi | safestack | sidestack | asan | sideasan> <envsetup | ssl | pcre | app | run | all>"
     exit 1
 fi
 
@@ -14,11 +14,15 @@ ROOT_DIR="$(cd -- "${SCRIPT_DIR}/.." &> /dev/null && pwd)"
 LLVM_DIR="${ROOT_DIR}/sidecar-llvm"
 BUILD_DIR="${ROOT_DIR}/build"
 INSTALL_DIR="${ROOT_DIR}/install/llvm-sidecar"
+CLEAN_DIR="${ROOT_DIR}/install/llvm-orig"
 SRC_DIR="${ROOT_DIR}/sidecar-benchmarks/lighttpd"
 HTTPD_DIR="${ROOT_DIR}/sidecar-benchmarks/apache"
 LLVM_BIN="${INSTALL_DIR}/bin"
 CC="${LLVM_BIN}/clang"
 CXX="${LLVM_BIN}/clang++"
+CLEAN_BIN="${CLEAN_DIR}/bin"
+CLEAN_CC="${CLEAN_BIN}/clang"
+CLEAN_CXX="${CLEAN_BIN}/clang++"
 
 # Installation folders
 APP_DIR="/usr/local/lighttpd-${MODE}"
@@ -31,13 +35,16 @@ LIGHTTPD_CONF="${SRC_DIR}/lighttpd.conf"
 # Flags
 LTO_FLAGS="-fvisibility=default -flto"
 CFI_FLAGS="-fsanitize=cfi-vcall,cfi-icall -fsanitize-cfi-cross-dso"
-SIDE_FLAGS="-fsanitize-cfi-decouple"
-SIDEX_FLAGS="-fsanitize-cfi-slowpath-decouple"
+SIDECFI_FLAGS="-fsanitize-cfi-decouple"
+SCS_FLAGS="-fsanitize=shadow-call-stack"
+SIDESTK_FLAGS="-fsanitize-sidestack"
+ASAN_FLAGS="-fsanitize=address -fsanitize-recover=all"
+SIDEASAN_FLAGS="-mllvm -asan-decouple"
 
 # Function for setting up environment variables
 setup_env() {
     export CC=$CC
-    export CXX=$CCX
+    export CXX=$CXX
     
     case $MODE in
         lto)
@@ -49,12 +56,26 @@ setup_env() {
             export CXXFLAGS="$LTO_FLAGS $CFI_FLAGS"
             ;;
         sidecfi)
-            export CFLAGS="$LTO_FLAGS $CFI_FLAGS $SIDE_FLAGS"
-            export CXXFLAGS="$LTO_FLAGS $CFI_FLAGS $SIDE_FLAGS"
+            export CFLAGS="$LTO_FLAGS $CFI_FLAGS $SIDECFI_FLAGS"
+            export CXXFLAGS="$LTO_FLAGS $CFI_FLAGS $SIDECFI_FLAGS"
             ;;
-        sidexcfi)
-            export CFLAGS="$LTO_FLAGS $CFI_FLAGS $SIDEX_FLAGS"
-            export CXXFLAGS="$LTO_FLAGS $CFI_FLAGS $SIDEX_FLAGS"
+        safestack)
+            export CFLAGS="$LTO_FLAGS $SCS_FLAGS"
+            export CXXFLAGS="$LTO_FLAGS $SCS_FLAGS"
+            ;;
+        sidestack)
+            export CFLAGS="$LTO_FLAGS $SCS_FLAGS $SIDESTK_FLAGS"
+            export CXXFLAGS="$LTO_FLAGS $SCS_FLAGS $SIDESTK_FLAGS"
+            ;;
+        asan)
+            export CFLAGS="$LTO_FLAGS $ASAN_FLAGS"
+            export CXXFLAGS="$LTO_FLAGS $ASAN_FLAGS"
+	    export CC=$CLEAN_CC
+	    export CXX=$CLEAN_CXX
+            ;;
+        sideasan)
+            export CFLAGS="$LTO_FLAGS $ASAN_FLAGS $SIDEASAN_FLAGS"
+            export CXXFLAGS="$LTO_FLAGS $ASAN_FLAGS $SIDEASAN_FLAGS"
             ;;
         *)
             echo "Invalid mode: $MODE"
