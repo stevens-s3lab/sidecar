611a8926f6847c9ccb1011bb1606fa134d21cbec
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-08-26 18:33:31 -0400

    Call all build scripts and more.

--- tools/build_fig9.py
+++ tools/build_fig9.py
@@ -3,21 +3,31 @@ import subprocess
 # List of values to use as arguments
 build_types = ["lto", "cfi", "sidecfi", "safestack", "sidestack", "asan", "sideasan"]
 
-# Loop over each build type and execute the script
+# List of build scripts to execute
+build_scripts = [
+    "./tools/build_bind.sh",
+    "./tools/build_httpd.sh",
+    "./tools/build_lighttpd.sh",
+    "./tools/build_memcached.sh",
+]
+
+# Loop over each build type and each script
 for build_type in build_types:
-    try:
-        print(f"Building lighttpd with {build_type}...")
-        # Execute the script with the current build type
-        result = subprocess.run(
-            ["./tools/build_lighttpd.sh", build_type, "all"],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        print(f"Build completed for {build_type}.\n")
-        print(result.stdout.decode("utf-8"))
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while building {build_type}.")
-        print(e.stderr.decode("utf-8"))
+    for build_script in build_scripts:
+        try:
+            script_name = build_script.split("/")[-1]
+            print(f"Building {script_name} with {build_type}...")
+            # Execute the script with the current build type
+            result = subprocess.run(
+                [build_script, build_type, "all"],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            print(f"Build completed for {script_name} with {build_type}.\n")
+            print(result.stdout.decode("utf-8"))
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred while building {script_name} with {build_type}.")
+            print(e.stderr.decode("utf-8"))
 
 print("All builds completed.")
