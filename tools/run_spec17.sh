f83a024554b340411a3851c94dc1c8d21f3a46f3
kleftog <kkleftog@gmail.com>
2024-08-27 19:31:45 -0400

    Fix spec run tool.

--- tools/run_spec17.sh
+++ tools/run_spec17.sh
@@ -9,13 +9,13 @@ modes=("lto" "cfi" "sidecfi" "safestack" "sidestack" "asan" "sideasan")
 size="ref"
 
 # Define the base directory where the Runxxx directories are located
-base_dir="../sidecar-results/raw"
+base_dir="$SCRIPT_DIR/../sidecar-results/raw"
 
 # Find the last Runxxx directory
 next_run=$(ls -1 "$base_dir" | grep -E '^Run[0-9]{3}$' | sort | tail -n 1)
 
 # Spec2017 directory
-spec17_dir="$SCRIPT_DIR/../sidecar-benchmarks/spec17"
+spec17_dir="$SCRIPT_DIR/../sidecar-benchmarks/spec2017"
 
 # cd to spec17 directory run ./shrc and then come back
 cd "$spec17_dir" || exit
@@ -32,11 +32,10 @@ for mode in "${modes[@]}"; do
 
     # Run the spec17 benchmark
     taskset -c 0 runcpu --action=run --config=$mode --size=$size --label=$mode \
-      --iterations=1 --threads=1 --tune=base --output_format=csv \
-      --nobuild speedint
+      --iterations=1 --threads=1 --tune=base -define gcc_dir=${llvm_path} --output_format=csv \
+      --noreportable speedint
 
-    # Fine the latest csv file in spec17 directory under result
-    csv_file=$(ls -1 "$spec17_dir/result" | grep -E '^speedint-[0-9]{3}-[0-9]{3}.csv$' | sort | tail -n 1)
-
-    grep "${size}speed(${size})" "$csv_file" > "$base_dir/$next_run/$(basename "$file")"
+    # Find the latest csv file in spec17 directory under result
+    csv_file=$(ls -1t "$spec17_dir/result/"*intspeed*.csv | head -n 1)
+    grep "refspeed(ref)" "$csv_file" > "$base_dir/$next_run/$mode.csv"
 done
