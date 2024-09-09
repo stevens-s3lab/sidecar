4a37c38918f7b64f94e9f4cb1540e739fa9e9c8b
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-09-09 09:36:03 -0400

    Use existing wq to sync monitor and producer threads.

--- sidecar-driver/x86-64/ptw.c
+++ sidecar-driver/x86-64/ptw.c
@@ -567,6 +567,9 @@ static long ptw_ioctl(struct file *filp, unsigned int cmd,
 		debugk("producer %d pinned to cpu %d\n", 				
 				dev->pid,
 				ptw_asan_cpu);
+
+    wait_event_interruptible(wq, dev->mid != -1);
+
 		return 0;
 	case PTW_DISABLE:
 		/* set pid */
@@ -579,11 +582,6 @@ static long ptw_ioctl(struct file *filp, unsigned int cmd,
 
 		return 0;
 	case PTW_SET_MID:
-		/* set monitor id */
-    dev->mid = current->pid;
-		debugk("monitor %d set\n", 				
-				dev->mid);
-
 		if (ptw_asan_cpu == -1) {
 			debugk("waiting for producer\n");
 
@@ -591,7 +589,14 @@ static long ptw_ioctl(struct file *filp, unsigned int cmd,
 			wait_event_interruptible(wq, ptw_asan_cpu > -1);
 			if (ptw_asan_cpu == -1) return 0;
 		} 
+
+		/* set monitor id */
 		dev = per_cpu(cpu_dev, ptw_asan_cpu);
+		dev->mid = current->pid;
+		debugk("monitor %d set\n", 				
+				dev->mid);
+
+    wake_up_interruptible(&wq);
 
 		return 0;
 	case PTW_GET_TOPA_SZ:
