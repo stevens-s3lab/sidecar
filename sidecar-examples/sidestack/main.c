0248a6f8c53f46daeea35cc9b23290e185fe7305
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-10-02 11:47:21 -0400

    Fixed example.

--- sidecar-examples/sidestack/main.c
+++ sidecar-examples/sidestack/main.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function that will be called normally
-__attribute__((noinline))
-void target_function() {
-    printf("Inside target_function, should return normally.\n");
-}
-
 // Function we will jump to by tampering the return address
 __attribute__((noinline))
 void other_function() {
     printf("Inside other_function, but we tampered the return address to get here!\n");
 }
 
-// Function to tamper with the return address of target_function
-void tamper_return_address() {
-    // Get the current frame pointer (x86_64 specific)
-    void **frame_pointer;
-    asm volatile ("movq %%rbp, %0" : "=r"(frame_pointer));
+// Function that will be tampered with
+__attribute__((noinline))
+void target_function(int tamper) {
+    printf("Inside target_function, should return normally.\n");
 
-    // Overwrite the return address (right above the frame pointer)
-    void **return_address = frame_pointer + 1;
+    if (tamper) {
+        // Overwrite the return address on the stack
+        void **return_address = (void **)__builtin_frame_address(0) + 1;
 
-    // Set the return address to the address of 'other_function'
-    printf("Tampering return address to point to other_function at %p\n", (void*)other_function);
-    *return_address = (void*)other_function; // Make the return go to a legit function
+        // Set the return address to point to 'other_function'
+        printf("Tampering return address to point to other_function at %p\n", (void*)other_function);
+        *return_address = (void*)other_function; // Change the return address
+    }
 }
 
-__attribute__((no_sanitize("shadow-call-stack"))) // Disable shadow call stack for main
 int main(int argc, char **argv) {
     if (argc != 2) {
         printf("Usage: %s <1 or 2>\n", argv[0]);
@@ -35,13 +29,12 @@ int main(int argc, char **argv) {
     }
 
     if (atoi(argv[1]) == 1) {
-        printf("Triggering shadow call stack violation by tampering return address.\n");
-        tamper_return_address(); // Modify return address to a legit function (other_function)
+        printf("Triggering shadow call stack violation by tampering return address...\n");
+        target_function(1); // Call target_function with tampering
     } else {
         printf("Running normally, no violation.\n");
+        target_function(0); // Call target_function without tampering
     }
-
-    target_function(); // This should return to 'other_function' if tampered
 
     return 0;
 }
