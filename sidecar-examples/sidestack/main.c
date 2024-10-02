37a1a8c5114609a538995153766c706f8c8fff16
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-10-02 10:42:20 -0400

    New test for scs.

--- sidecar-examples/sidestack/main.c
+++ sidecar-examples/sidestack/main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <asm/prctl.h>        /* Definition of ARCH_* constants */
 #include <sys/syscall.h>      /* Definition of SYS_* constants */
 #include <unistd.h>
 #include <syscall.h>
 
-
 __attribute__((noinline))
-static int
-leaf(int n)
+static int leaf(int n)
 {
 	return n * 2 - 127;
+}
+
+__attribute__((noinline))
+static int tamper_return_address()
+{
+	// Get the current frame pointer (x86_64 specific code)
+	void **frame_pointer;
+	asm volatile ("movq %%rbp, %0" : "=r"(frame_pointer));
+
+	// Overwrite the return address (two levels up)
+	void **return_address = frame_pointer + 1;  // Move to the return address
+	printf("Tampering return address from %p to NULL\n", *return_address);
+	*return_address = NULL;  // Set it to NULL or an invalid address to cause a crash
+	return 0;
 }
 
 __attribute__((noinline))
@@ -20,7 +32,6 @@ static int bar(int n)
 {
 	return leaf(n);
 }
-
 
 __attribute__((noinline))
 static int foobar(int n)
@@ -52,27 +63,11 @@ static int root(int n, int (*fptr)(int))
 
 #define arch_prctl(code, addr) syscall(SYS_arch_prctl, (code), (addr))
 
-
 __attribute__((no_sanitize("shadow-call-stack")))
 int main(int argc, char **argv) 
 {
 	long gs;
 	int (*fptr)(int);
-
-#if 0
-	void *scs = mmap(NULL, 8192, PROT_READ|PROT_WRITE,
-			MAP_PRIVATE|MAP_ANON, -1, 0);
-	if (scs == MAP_FAILED) {
-		perror("mmap");
-		return -1;
-	}
-	printf("Mapped SCS at %8p\n", scs);
-
-	if (arch_prctl(ARCH_SET_GS, scs) != 0) {
-		perror("arch_prctl");
-		return -1;
-	}
-#endif
 
 	if (arch_prctl(ARCH_GET_GS, &gs) != 0) {
 		perror("arch_prctl");
@@ -89,13 +84,18 @@ int main(int argc, char **argv)
 	} else {
 		fptr = foobar;
 	} 
+
+	// Trigger the test based on the argument
+	if (argc > 1 && atoi(argv[1]) == 1) {
+		printf("Triggering shadow call stack violation by tampering with the return address.\n");
+		tamper_return_address();
+	} else {
+		printf("Running normally, no violation.\n");
+	}
+
+	// Call the function, which might have its return address tampered with
 	root(argc, fptr);
 	fptr(0);
-#if 0
-	if (arch_prctl(ARCH_SET_GS, 0) != 0) {
-		perror("arch_prctl");
-		return -1;
-	}
-#endif
+
 	return 0;
 }
