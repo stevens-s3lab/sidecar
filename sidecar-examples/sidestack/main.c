95588a9fbe6fa5315e6f5f227b8d877fb782cf27
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-10-02 10:50:40 -0400

    New example.

--- sidecar-examples/sidestack/main.c
+++ sidecar-examples/sidestack/main.c
@@ -1,101 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <asm/prctl.h>        /* Definition of ARCH_* constants */
-#include <sys/syscall.h>      /* Definition of SYS_* constants */
+#include <asm/prctl.h>
+#include <sys/syscall.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <syscall.h>
-
-__attribute__((noinline))
-static int leaf(int n)
-{
-	return n * 2 - 127;
-}
-
-__attribute__((noinline))
-static int tamper_return_address()
-{
-	// Get the current frame pointer (x86_64 specific code)
-	void **frame_pointer;
-	asm volatile ("movq %%rbp, %0" : "=r"(frame_pointer));
-
-	// Overwrite the return address (two levels up)
-	void **return_address = frame_pointer + 1;  // Move to the return address
-	printf("Tampering return address from %p to NULL\n", *return_address);
-	*return_address = NULL;  // Set it to NULL or an invalid address to cause a crash
-	return 0;
-}
-
-__attribute__((noinline))
-static int bar(int n)
-{
-	return leaf(n);
-}
-
-__attribute__((noinline))
-static int foobar(int n)
-{
-	n = leaf(n);
-	return bar(n);
-}
-
-__attribute__((noinline))
-static int barfoo(int n)
-{
-	printf("What barfood %d is this\n", n);
-	return foobar(n);
-}
-
-static int foo(int n)
-{
-	n = leaf(n);
-	return printf("What foo %d is this\n", bar(n));
-}
-
-static int root(int n, int (*fptr)(int))
-{
-	if (n != 1) {
-		printf("yowza %d\n", n);
-	}
-	return fptr(n);
-}
 
 #define arch_prctl(code, addr) syscall(SYS_arch_prctl, (code), (addr))
 
-__attribute__((no_sanitize("shadow-call-stack")))
-int main(int argc, char **argv) 
-{
-	long gs;
-	int (*fptr)(int);
+// Function that will be tampered with
+__attribute__((noinline))
+void test_function() {
+    printf("Inside test_function\n");
+}
 
-	if (arch_prctl(ARCH_GET_GS, &gs) != 0) {
-		perror("arch_prctl");
-		return -1;
-	}
-	printf("Hello world gs=%lx!\n", gs);
+// Function to tamper with return address
+void tamper_return_address() {
+    // Access the current frame pointer (x86_64 specific)
+    void **frame_pointer;
+    asm volatile ("movq %%rbp, %0" : "=r"(frame_pointer));
 
-	if (argc == 1) {
-		fptr = foo; 
-	} else if (argc == 2) {
-		fptr = bar;
-	} else if (argc == 3) {
-		fptr = barfoo;
-	} else {
-		fptr = foobar;
-	} 
+    // Overwrite the return address (which is right above the frame pointer)
+    void **return_address = frame_pointer + 1;
+    printf("Tampering return address from %p to NULL\n", *return_address);
+    *return_address = NULL; // Set it to an invalid address to trigger a violation
+}
 
-	// Trigger the test based on the argument
-	if (argc > 1 && atoi(argv[1]) == 1) {
-		printf("Triggering shadow call stack violation by tampering with the return address.\n");
-		tamper_return_address();
-	} else {
-		printf("Running normally, no violation.\n");
-	}
+__attribute__((no_sanitize("shadow-call-stack"))) // Disable shadow call stack for main
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <1 or 2>\n", argv[0]);
+        return 1;
+    }
 
-	// Call the function, which might have its return address tampered with
-	root(argc, fptr);
-	fptr(0);
+    if (atoi(argv[1]) == 1) {
+        printf("Triggering shadow call stack violation...\n");
+        tamper_return_address(); // Tamper with the return address
+    } else {
+        printf("Running normally, no violation.\n");
+    }
 
-	return 0;
+    test_function(); // Call the function (return address will be tampered with in case 1)
+
+    return 0;
 }
