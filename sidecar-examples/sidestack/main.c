2337faca21145652610fbe2a4fabfe38801e1486
Koss Kleftogiorgos <kkleftog@gmail.com>
2024-10-02 11:40:08 -0400

    New example.

--- sidecar-examples/sidestack/main.c
+++ sidecar-examples/sidestack/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function that will be called and where we will tamper the return address
+// Function that will be called normally
 __attribute__((noinline))
 void target_function() {
     printf("Inside target_function, should return normally.\n");
+}
+
+// Function we will jump to by tampering the return address
+__attribute__((noinline))
+void other_function() {
+    printf("Inside other_function, but we tampered the return address to get here!\n");
 }
 
 // Function to tamper with the return address of target_function
@@ -13,12 +19,12 @@ void tamper_return_address() {
     void **frame_pointer;
     asm volatile ("movq %%rbp, %0" : "=r"(frame_pointer));
 
-    // Overwrite the return address (the value right above the frame pointer)
+    // Overwrite the return address (right above the frame pointer)
     void **return_address = frame_pointer + 1;
 
-    // Set the return address to some invalid location (NULL in this case)
-    printf("Tampering return address from %p to NULL\n", *return_address);
-    *return_address = (void*)0xdeadbeef; // Jump to an invalid address
+    // Set the return address to the address of 'other_function'
+    printf("Tampering return address to point to other_function at %p\n", (void*)other_function);
+    *return_address = (void*)other_function; // Make the return go to a legit function
 }
 
 __attribute__((no_sanitize("shadow-call-stack"))) // Disable shadow call stack for main
@@ -30,12 +36,12 @@ int main(int argc, char **argv) {
 
     if (atoi(argv[1]) == 1) {
         printf("Triggering shadow call stack violation by tampering return address.\n");
-        tamper_return_address(); // Modify return address to invalid location
+        tamper_return_address(); // Modify return address to a legit function (other_function)
     } else {
         printf("Running normally, no violation.\n");
     }
 
-    target_function(); // This should trigger the violation if return address is tampered
+    target_function(); // This should return to 'other_function' if tampered
 
     return 0;
 }
