d8af527b1a784c1cf2be6d4a28be44d300890412
kleftog <kkleftog@gmail.com>
2024-10-08 08:15:34 -0400

    Remove unused.

--- sidecar-llvm/compiler-rt/test/asan/TestCasesDecoupled/Linux/asan_default_suppressions.cpp
+++
@@ -1,7 +0,0 @@
-// Test that we use the suppressions from __asan_default_suppressions.
-// RUN: %clangxx_asan %s -o %t && /home/kleftog/sidecar-ae/sidecar/sidecar-monitors/sideasan/x86-64/monitor & not %run %t 2>&1 | FileCheck %s
-extern "C" {
-  const char *__asan_default_suppressions() { return "FooBar"; }
-}
-// CHECK: AddressSanitizer: failed to parse suppressions
-int main() {}
